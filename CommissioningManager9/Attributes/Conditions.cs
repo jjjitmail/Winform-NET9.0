decfd92b77b2a202c40483ccc26d8065e025abad
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-14 22:29:44 +0100

    Add files via upload

---
+++ CommissioningManager9/Attributes/Conditions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Property)]
+    public class Conditions : Attribute
+    {
+        private bool _Compare;
+        public bool Compare
+        {
+            get { return _Compare; }
+            set { _Compare = value; }
+        }
+
+        private bool _CompareRequired;
+        public bool CompareRequired
+        {
+            get { return _CompareRequired; }
+            set { _CompareRequired = value; }
+        }
+
+        private bool _CompareRequiredSame;
+        public bool CompareRequiredSame
+        {
+            get { return _CompareRequiredSame; }
+            set { _CompareRequiredSame = value; }
+        }
+
+        private bool _CompareRequiredSameField;
+        public bool CompareRequiredSameField
+        {
+            get { return _CompareRequiredSameField; }
+            set { _CompareRequiredSameField = value; }
+        }
+
+        private bool _Required;
+        public bool Required
+        {
+            get { return _Required; }
+            set { _Required = value; }
+        }
+
+        private Type _ExpectedType;
+        public Type ExpectedType
+        {
+            get { return _ExpectedType; }
+            set { _ExpectedType = value; }
+        }
+
+        private int _MaxLength;
+        public int MaxLength
+        {
+            get { return _MaxLength; }
+            set { _MaxLength = value; }
+        }
+
+        private bool _ReadOnly;
+        public bool ReadOnly
+        {
+            get { return _ReadOnly; }
+            set { _ReadOnly = value; }
+        }
+
+        private string _DisplayName;
+        public string DisplayName
+        {
+            get { return _DisplayName; }
+            set { _DisplayName = value; }
+        }
+
+        private Type _ConverterType;
+        public Type ConverterType
+        {
+            get { return _ConverterType; }
+            set { _ConverterType = value; }
+        }
+    }
+}
---
+++ CommissioningManager9/Attributes/StringValueAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace Attributes
+{
+    public class StringValueAttribute : Attribute
+    {
+        private string _value;
+
+        public StringValueAttribute(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }        
+    }   
+}
