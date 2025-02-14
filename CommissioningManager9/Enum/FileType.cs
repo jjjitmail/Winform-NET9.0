6b901330e85728c0033f2f9a6204424893fbdcb0
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-14 22:37:04 +0100

    Add files via upload

---
+++ CommissioningManager9/Enum/FileType.cs
@@ -0,0 +1,19 @@
+﻿using Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum FileType
+    {
+        [StringValue("*.accdb")]
+        LuxData,
+        [StringValue("*.icf")]
+        ScanData,
+        [StringValue("*.jsn")]
+        TeleControllerData
+    }
+}
