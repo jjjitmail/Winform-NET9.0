5ef7ac96ddbbe4f7b181ede56eac34d3fc88efe8
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-14 22:38:08 +0100

    Add files via upload

---
+++ CommissioningManager9/Helpers/ControlExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+//using System.Windows.Forms;
+
+namespace Helpers
+{
+    public static class ControlExtension
+    {
+        public static IEnumerable<string> GetRandomString(this List<string> numberOfRandoms, int length)
+        {
+            var allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+
+            var unique = new HashSet<string>();
+            var rd = new Random();
+
+            var chars = new char[length];
+
+            for (int i = 0; i < numberOfRandoms.Count + 10; i++)
+            {
+                for (var j = 0; j < length; j++)
+                {
+                    chars[j] = allowedChars[rd.Next(0, allowedChars.Length)];                    
+                }
+                if (!unique.Contains(new String(chars)))
+                    unique.Add(new String(chars));
+            }
+            
+            return unique.ToList();
+        }
+
+        
+    }
+}
---
+++ CommissioningManager9/Helpers/Utls.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Helpers
+{
+    public static class Utls<T> where T : class
+    {
+        public static void SaveToXML(T obj, string fileName)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(ms))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    serializer.Serialize(writer, obj);
+                    writer.Flush();
+                    if (File.Exists(fileName))
+                    {
+                        File.Delete(fileName);
+                    }
+                    File.WriteAllBytes(fileName, ms.ToArray());
+                }
+            }
+        }
+
+        public static T LoadFromXML(string fileName)
+        {
+            using (var stream = File.OpenRead(fileName))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                return serializer.Deserialize(stream) as T;
+            }
+        }
+    }
+}
